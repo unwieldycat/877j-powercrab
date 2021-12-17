f284a031186e6df9977d2bd9f43a46f96b1be9aa
unwieldycat <thurstonalexyates@gmail.com>
2021-12-17 08:46:12 -0500

    Reset font after draw

--- src/main.cpp
+++ src/main.cpp
@@ -81,6 +81,7 @@ namespace ui {
           // Reset colors
           Brain.Screen.setFillColor(transparent);
           Brain.Screen.setPenColor(white);
+          Brain.Screen.setFont(vex::fontType::mono20);
         }
     };
 
