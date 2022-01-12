7b618866e7006dc97a949773bb48001dc1cee39c
unwieldycat <thurstonalexyates@gmail.com>
2022-01-12 15:22:12 -0500

    Move autonomous manager to seperate file

---
+++ include/autonutils.h
@@ -0,0 +1,65 @@
+#ifndef _AUTONUTILS_
+#define _AUTONUTILS_
+
+// ================================ Imports ================================ //
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+// ====================== Autonomous Manager (Headers) ====================== //
+
+namespace autonutils {
+    /**
+     * @brief Side the autonomous routine starts on
+     */
+    enum FieldOrigin {
+        Left,
+        Right,
+        Both
+    };
+
+    /**
+     * @brief Routine struct
+     */
+    struct Routine
+    {
+        int id;
+        int side;
+        std::function<void()> action;
+    };
+
+    class RoutineManager
+    {
+    private:
+        std::vector<Routine> routines;
+
+    public:
+        RoutineManager(){};
+
+        /**
+         * @brief Find ids of routines that start from a specified side
+         * @param s Side
+         * @returns A vector of ids
+         */
+        std::vector<int> find(int s);
+
+        /**
+         * @brief Add a routine to storage
+         * @param i Id of routine
+         * @param s Side routine starts on
+         * @param a Routine action
+         */
+        void add(int i, int s, std::function<void()> a);
+
+        /**
+         * @brief Run a routine with id
+         * @param i Id of routine to run
+         */
+        void exec(int i);
+    };
+}
+
+// ========================================================================= //
+
+#endif // _AUTONUTILS_
---
+++ src/autonutils.cpp
@@ -0,0 +1,59 @@
+// ================================ Imports ================================ //
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+// ========================== Autonomous Utilities ========================== //
+
+namespace autonutils {
+    enum FieldOrigin {
+        Left,
+        Right,
+        Both
+    };
+
+    struct Routine
+    {
+        int id;
+        int side;
+        std::function<void()> action;
+    };
+
+    class RoutineManager
+    {
+    private:
+        std::vector<Routine> routines;
+
+    public:
+        RoutineManager() {}
+
+        std::vector<int> find(int s)
+        {
+            std::vector<int> matches;
+            for (Routine &r : routines)
+            {
+                if (r.side == s)
+                    matches.push_back(r.id);
+            }
+            return matches;
+        }
+
+        void add(int i, int s, std::function<void()> a)
+        {
+            Routine r;
+            r.id = i, r.side = s;
+            r.action = a;
+            routines.push_back(r);
+        }
+
+        void exec(int i)
+        {
+            for (Routine &r : routines)
+            {
+                if (r.id == i)
+                    r.action();
+            }
+        }
+    };
+}
